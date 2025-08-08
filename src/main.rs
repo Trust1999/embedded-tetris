6335d0245a964059153a2f167f350ccd496c1895
Nika Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2025-08-08 21:14:01 +0200

    Add driver for max72xx matrix displays.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use embedded_hal::spi::SpiDevice;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::spi;
 use std::time::Duration;
 
 mod game;
 use game::TetrisGame;
 
-fn main() {
+mod ma72xx;
+use ma72xx::Max72xx;
+
+fn setup() -> anyhow::Result<(Max72xx<impl SpiDevice<Error = spi::SpiError>>, ())> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -11,11 +17,49 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let mut game = TetrisGame::new();
+    let peripherals = Peripherals::take().unwrap();
 
-    for i in 0.. {
-        let display = game.step(i);
-        println!("{}", display);
-        std::thread::sleep(Duration::from_millis(500));
+    // Initialize SPI2
+    let spi_driver = spi::SpiDriver::new(
+        peripherals.spi2,
+        peripherals.pins.gpio12,       // SCLK (FSPICLK)
+        peripherals.pins.gpio11,       // MOSI (FSPID)
+        Some(peripherals.pins.gpio13), // MISO (FSPIQ), not used
+        &Default::default(),
+    )?;
+
+    // Chip Select pin for the cascaded MAX72xx devices
+    let cs_pin = peripherals.pins.gpio10;
+    let spi = spi::SpiDeviceDriver::new(spi_driver, Some(cs_pin), &Default::default())?;
+
+    let mut max = Max72xx::new(spi, 4);
+    max.reset()?;
+
+    std::thread::sleep(Duration::from_millis(100));
+
+    Ok((max, ()))
+}
+
+fn main() -> anyhow::Result<()> {
+    let (mut max, _) = setup()?;
+
+    let mut i: usize = 0;
+    loop {
+        if i == 8 * 8 * 4 {
+            max.fill(false);
+            i = 0;
+        }
+        max.set_pixel((i % 8) as u8, (i / 8) as u8, true);
+        max.write_bitmap()?;
+        // std::thread::sleep(Duration::from_millis(10));
+        i += 1;
     }
+
+    // let mut game = TetrisGame::new();
+
+    // for i in 0.. {
+    //     let display = game.step(i);
+    //     println!("{}", display);
+    //     std::thread::sleep(Duration::from_millis(500));
+    // }
 }
