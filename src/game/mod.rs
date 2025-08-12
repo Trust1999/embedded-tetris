95e47d2267b286751f85c5d9c1f9ea6d41028e16
Nika Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2025-08-12 20:49:15 +0200

    Remove full rows of blocks.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -15,6 +15,16 @@ pub struct InGameState {
     time_last_move: u64,
 }
 
+impl GameState {
+    pub fn update(self, time: &Time) -> Self {
+        match self {
+            GameState::StartMenu => todo!(),
+            GameState::InGame(state) => state.update(time),
+            GameState::GameOverMenu => todo!(),
+        }
+    }
+}
+
 impl InGameState {
     pub fn new() -> Self {
         Self {
@@ -26,47 +36,40 @@ impl InGameState {
             time_last_move: 0,
         }
     }
-}
 
-impl GameState {
-    pub fn update(self, time: &Time) -> Self {
-        match self {
-            GameState::StartMenu => todo!(),
-            GameState::InGame(state) => Self::update_in_game(state, time),
-            GameState::GameOverMenu => todo!(),
-        }
-    }
-
-    fn update_in_game(mut state: InGameState, time: &Time) -> Self {
-        if (time.now_ms() - state.time_last_move) < 250 {
-            return Self::InGame(state);
+    fn update(mut self, time: &Time) -> GameState {
+        if (time.now_ms() - self.time_last_move) < 250 {
+            return GameState::InGame(self);
         }
 
-        state.time_last_move = time.now_ms();
-        state.current_piece.move_by(0, 1);
+        self.time_last_move = time.now_ms();
+        self.current_piece.move_by(0, 1);
 
-        if state.next_piece.is_none() && state.current_piece.position_y > 8 {
-            state.next_piece = Some(Piece::random());
+        if self.next_piece.is_none() && self.current_piece.position_y > 8 {
+            self.next_piece = Some(Piece::random());
         }
 
         // Collissions with floor, walls and existing blocks
-        let will_intersect = state.blocks.intersects(&state.current_piece);
-        if state.next_piece.is_some() && will_intersect {
+        let will_intersect = self.blocks.intersects(&self.current_piece);
+        if self.next_piece.is_some() && will_intersect {
             // Place piece ontop of existing blocks
-            for (x, y) in state.current_piece.filled_positions() {
-                state.blocks.set(x, y);
+            for (x, y) in self.current_piece.filled_positions() {
+                self.blocks.set(x, y);
             }
-            state.current_piece = state.next_piece.take().unwrap();
+            self.current_piece = self.next_piece.take().unwrap();
         }
 
         // Check if game is over
-        let blocks_reached_top = !state.blocks.rows().take(8).all(|row| row == 0x00);
+        let blocks_reached_top = !self.blocks.rows().take(8).all(|row| row == 0x00);
         if blocks_reached_top {
             log::info!("Game over");
-            return Self::GameOverMenu;
+            return GameState::GameOverMenu;
         }
 
-        Self::InGame(state)
+        // Remove full rows of blocks
+        let points = self.blocks.remove_full_rows() * 100;
+
+        GameState::InGame(self)
     }
 }
 
@@ -75,8 +78,13 @@ struct Blocks {
 }
 
 impl Blocks {
-    fn intersects(&self, piece: &Piece) -> bool {
-        piece.filled_positions().any(|(x, y)| self.get(x, y))
+    fn get(&self, x: i16, y: i16) -> bool {
+        if x < 0 || x >= DISPLAY_WIDTH as i16 || y < 0 || y >= DISPLAY_HEIGHT as i16 {
+            return true;
+        }
+
+        let mask = 0b1000_0000 >> x;
+        self.data[y as usize] & mask != 0x00
     }
 
     fn set(&mut self, x: i16, y: i16) {
@@ -88,17 +96,36 @@ impl Blocks {
         self.data[y as usize] |= mask;
     }
 
-    fn get(&self, x: i16, y: i16) -> bool {
-        if x < 0 || x >= DISPLAY_WIDTH as i16 || y < 0 || y >= DISPLAY_HEIGHT as i16 {
-            return true;
-        }
-
-        let mask = 0b1000_0000 >> x;
-        self.data[y as usize] & mask != 0x00
-    }
-
     fn rows(&self) -> impl Iterator<Item = u8> {
         self.data.into_iter()
+    }
+
+    fn intersects(&self, piece: &Piece) -> bool {
+        piece.filled_positions().any(|(x, y)| self.get(x, y))
+    }
+
+    /// Returns how many rows were removed
+    fn remove_full_rows(&mut self) -> u32 {
+        let mut rows_removed = 0;
+
+        for (y, row) in self.data.into_iter().enumerate() {
+            // Skip rows that are not filled
+            if row != 0xff {
+                continue;
+            }
+
+            // Move all blocks above it down by one
+            for i in y..0 {
+                self.data[i] = self.data[i - 1];
+            }
+
+            // Clear first row
+            self.data[0] = 0xff;
+
+            rows_removed += 1;
+        }
+
+        rows_removed
     }
 }
 
